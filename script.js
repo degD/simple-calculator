df43cf4d9b00e853faf5d2cb675767383781e6dc
degD <108623049+degD@users.noreply.github.com>
2024-03-11 18:17:16 +0200

    implement basic operations of calculator

--- script.js
+++ script.js
@@ -3,107 +3,78 @@
 const calculatorDisplay = document.querySelector(".display");
 const calculatorButtons = document.querySelectorAll(".button");
 
-// global variables
-let buttonClickBuffer = [];
+// calculator globals
+let num1 = 0;
+let num2 = 0;
+let numBuffer = [];
+let numSign = "+";
+let currentOperation = "+";
 
 // add event listeners to buttons to capture button clicks
 calculatorButtons.forEach(button => {
     button.addEventListener("click", evt => {
         calculatorButtonEvaluator(button);
         console.log(`button press: ${evt.target.textContent}`);
-        console.log(`button buffer: ${buttonClickBuffer}`);
     })
 });
 
-// debug calculation test
-let testTokens = ["12", "+", "30", "*", "2"];
-simpleInfixEvaluation(testTokens);
-
+/**
+ * Function to evaluate calculator button presses
+ * @param {Element} button Button HTML element
+ */
 function calculatorButtonEvaluator(button) {
-    if (button.textContent === "=") {
-        let tokens = simpleMathLexer();
-        let result = simpleInfixEvaluation(tokens);
-        console.log(result);
+    let buttonText = button.textContent;
+    let operators = "/*-+=";
+    if (buttonText == "C") {
+        num1 = 0;
+        num2 = 0;
+        numBuffer = [];
+        numSign = "+";
+        currentOperation = "+";
+        updateDisplay();
     }
-    else if (button.textContent === "C") {
-        buttonClickBuffer = [];
-    }
-    else if (button.textContent === "CE") {
-        buttonClickBuffer.pop();
-    }
-    else {
-        buttonClickBuffer.push(button.textContent);
+    else if (Number.isInteger(+buttonText) || buttonText == ".") {
+        // if pressed a digit button or the point button
+        if (numBuffer.length == 0) {
+            updateDisplay();
+        }
+        numBuffer.push(buttonText);
+        updateDisplay(numBuffer.join(""));
+    } 
+    else if (operators.includes(buttonText)) {
+        num2 = +numBuffer.join("");
+        num1 = doMathOperation();
+        num2 = 0;
+        currentOperation = buttonText;
+        numBuffer = [];
+        updateDisplay(num1);
+        console.log(num1, num2, currentOperation);
     }
 }
 
-function simpleMathLexer() {
-    let num = "";
-    let mathTokens = [];
-    for (const ch of buttonClickBuffer) {
-        if ("/*-+".includes(ch)) {
-            mathTokens.push(num);
-            mathTokens.push(ch);
-            num = "";
-        }
-        else {
-            num += ch;
-        }
-    }
-    mathTokens.push(num);
-
-    console.log(mathTokens);
-    return mathTokens;
-}
-
-function simpleMathProcess(a, b, opr) {
-    switch (opr) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "*":
-            return a * b;
+/**
+ * Do calculations for 4 basic operations: /, *, -, +
+ * @returns {number} Result of the basic operation
+ */
+function doMathOperation() {
+    switch (currentOperation) {
         case "/":
-            return a / b;
-    }
-}
-
-function simpleInfixEvaluation(mathTokens) {
-    let numberStack = [];
-    let operatorStack = [];
-    let precedence = {"/": 2, "*": 2, "-": 1, "+": 1};
-    for (const token of mathTokens) {
-        if (isNaN(+token)) {
-            if (operatorStack.length == 0) {
-                operatorStack.push(token);
-            } else {
-                if (precedence[token] > precedence[operatorStack.at(-1)]) {
-                    operatorStack.push(token);
-                } else {
-                    while (operatorStack.length > 0) {
-                        let num2 = +numberStack.pop();
-                        let num1 = +numberStack.pop();
-                        let opr = operatorStack.pop();
-                        let result = simpleMathProcess(num1, num2, opr);
-                        console.log(`${num1}${opr}${num2}=${result}`);
-                        numberStack.push(result);
-                    }
-                }
+            if (num2 == 0) {
+                console.log("YOU CANNOT DIVIDE TO 0!");
+                return 42;
             }
-        } else {
-            numberStack.push(token);
-        }
-        console.log("number stack: " + numberStack);
-        console.log("operator stack: " + operatorStack);
+            return num1 / num2;
+        case "*":
+            return num1 * num2;
+        case "-":
+            return num1 - num2;
+        case "+":
+            return num1 + num2;
+        case "=":
+            return num1;
     }
-    while (operatorStack.length > 0) {
-        let num2 = +numberStack.pop();
-        let num1 = +numberStack.pop();
-        let result = simpleMathProcess(num1, num2, operatorStack.pop());
-        numberStack.push(result);
-    }
+} 
 
-    let finalResult = numberStack.at(0);
-    console.log("result: " + finalResult);
-    return finalResult;
+function updateDisplay(newText) {
+    calculatorDisplay.textContent = newText;
 }
